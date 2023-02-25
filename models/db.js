306fbf47dd3bc48f3529989c4afb30fe72e78d44
Никита <mnd1work@gmail.com>
2023-02-26 00:17:34 +0400

    commit 26.2.23 (1)

--- models/db.js
+++ models/db.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql2');
+const { Client } = require('pg'); 
 const env = require('dotenv').config();
 
-const connection = mysql.createConnection({
-    port: process.env.PORT_DB,
+const client = new Client({
     user: process.env.USER_DB,
     host: process.env.HOST_DB,
     database: process.env.DATABASE_DB,
-    password: process.env.PASSWORD_DB
+    password: process.env.PASSWORD_DB,
+    port: process.env.PORT_DB,
 });
 
-connection.connect(err => {
+client.connect(err => {
     if (err) throw error;
     
     console.log("Успешно соединено с базой данных");
 });
 
-module.exports = connection;
+module.exports = client;
