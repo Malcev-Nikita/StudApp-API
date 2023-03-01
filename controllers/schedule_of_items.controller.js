dfb16a9e1dcb8d0183af87ddf3d2ecd4f74b5f67
Malcev-Nikita <mnd1work@gmail.com>
2023-03-01 23:20:36 +0400

    commit 1.3.23 (2)

--- controllers/schedule_of_items.controller.js
+++ controllers/schedule_of_items.controller.js
@@ -8,7 +8,16 @@ class Schedule_Of_Items {
 
     async GetAllItemsOfGroup(req, res) {
         const group = req.params.group
-        db.query(`SELECT * FROM Schedule_Of_Items WHERE Id_Group = ${group}`, (error, result, fields) => res.json(result))
+        const request = `SELECT Id, (SELECT Name FROM Subject WHERE Id_Subject = Subject.Id) AS 'Subject', 
+                                    (SELECT Time_Start FROM Сall_Schedule WHERE Id_Call_Schedule = Сall_Schedule.Id) AS 'Time_Start',
+                                    (SELECT Time_End FROM Сall_Schedule WHERE Id_Call_Schedule = Сall_Schedule.Id) AS 'Time_End', 
+                                    (SELECT Name FROM Groups WHERE Id_Group = Groups.Id) AS 'Group', 
+                                    Day_Of_Week, 
+                                    Week_Number 
+                                        FROM Schedule_Of_Items 
+                                            WHERE Id_Group = ${group}`
+
+        db.query(request, (error, result, fields) => res.json(result))
     }
 
     async GetStudent(req, res) {
