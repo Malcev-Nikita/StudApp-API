003b3b371498a2c2920ebe9b954132590410d954
Malcev-Nikita <mnd1work@gmail.com>
2023-03-15 11:55:00 +0400

    commit 15.3.23 (1)

--- controllers/schedule_of_items.controller.js
+++ controllers/schedule_of_items.controller.js
@@ -4,9 +4,10 @@ class Schedule_Of_Items {
 
     async CountSubjectDay(req, res) {
         const group = req.params.group
-        const request = `SELECT Day_Of_Week, COUNT(*) AS "Count" 
+        const request = `SELECT (SELECT Name FROM Day_Of_Week WHERE Day_Of_Week_Id = Day_Of_Week.Id) AS "Day_Of_Week" 
                             FROM Schedule_Of_Items 
-                                WHERE Id_Group = ${group}` 
+                                WHERE Id_Group = ${group}
+                                    GROUP BY Day_Of_Week` 
                                 
         db.query(request, (error, result, fields) => res.json(result))
     }
@@ -16,9 +17,11 @@ class Schedule_Of_Items {
         const request = `SELECT Id, (SELECT Name FROM Subject WHERE Id_Subject = Subject.Id) AS 'Subject', 
                                     (SELECT Time_Start FROM Сall_Schedule WHERE Id_Call_Schedule = Сall_Schedule.Id) AS 'Time_Start',
                                     (SELECT Time_End FROM Сall_Schedule WHERE Id_Call_Schedule = Сall_Schedule.Id) AS 'Time_End', 
+                                    (SELECT Full_Name FROM Users WHERE 
+                                        (SELECT Users_Id FROM Teacher WHERE Teacher_Id = Teacher.Id) = Users.Id) AS 'Teacher', 
                                     (SELECT Name FROM Groups WHERE Id_Group = Groups.Id) AS 'Group', 
-                                    Day_Of_Week, 
-                                    Week_Number 
+                                    (SELECT Name FROM Day_Of_Week WHERE Day_Of_Week_Id = Day_Of_Week.Id) AS 'Day_Of_Week', 
+                                    Number_Of_Week 
                                         FROM Schedule_Of_Items 
                                             WHERE Id_Group = ${group}`
 
