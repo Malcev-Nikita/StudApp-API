1d4f88a741c11653feaeec3b95dd7a10a1ad3f8a
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-05-01 23:35:19 +0400

    commit 1.4.23 (2)

--- controllers/schedule_of_items.controller.js
+++ controllers/schedule_of_items.controller.js
@@ -14,15 +14,16 @@ class Schedule_Of_Items {
 
     async GetAllItemsOfGroup(req, res) {
         const group = req.params.group
-        const request = `SELECT Id, (SELECT Name FROM Subject WHERE Id_Subject = Subject.Id) AS 'Subject', 
-                                    (SELECT Time_Start FROM Сall_Schedule WHERE Id_Call_Schedule = Сall_Schedule.Id) AS 'Time_Start',
-                                    (SELECT Time_End FROM Сall_Schedule WHERE Id_Call_Schedule = Сall_Schedule.Id) AS 'Time_End', 
-                                    (SELECT Full_Name FROM Users WHERE 
-                                    (SELECT Users_Id FROM Teacher WHERE Teacher_Id = Teacher.Id) = Users.Id) AS 'Teacher', 
-                                    (SELECT Name FROM Groups WHERE Id_Group = Groups.Id) AS 'Group', 
-                                    (SELECT Name FROM Day_Of_Week WHERE Day_Of_Week_Id = Day_Of_Week.Id) AS 'Day_Of_Week', 
-                                    Number_Of_Week 
-                                        FROM Schedule_Of_Items 
+        const request = `SELECT Id, 
+                            (SELECT Name FROM Subject WHERE Id_Subject = Subject.Id) AS 'Subject', 
+                            (SELECT Time_Start FROM Сall_Schedule WHERE Call_Schedule_Id = Сall_Schedule.Id) AS 'Time_Start',
+                            (SELECT Time_End FROM Сall_Schedule WHERE Call_Schedule_Id = Сall_Schedule.Id) AS 'Time_End', 
+                            (SELECT Full_Name FROM Users WHERE 
+                            (SELECT Id FROM Users WHERE Teacher_Id = Users.Id) = Users.Id) AS 'Users', 
+                            (SELECT Name FROM Group WHERE Group_Id = Group.Id) AS 'Group', 
+                            (SELECT Name FROM Day_Of_Week WHERE Day_Of_Week_Id = Day_Of_Week.Id) AS 'Day_Of_Week', 
+                            A_Week_In_A_Row 
+                                FROM Schedule_Of_Items  
                                             WHERE Id_Group = ${group}`
 
         db.query(request, (error, result, fields) => res.json(result))
